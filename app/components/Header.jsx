b93bf725fbff1d6f24530b760c2c4a83017b43f4
arijit203 <arijit.saha1373@gmail.com>
2024-08-05 19:48:34 +0530

    last_commit_2.0

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,31 +1,40 @@
-import { UserButton } from '@clerk/nextjs';
-import Image from 'next/image';
-import React from 'react';
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import React from "react";
 
 function Header() {
   const headerMenu = [
     {
       id: 1,
-      name: 'Ride',
-      icon: '/ambulance4.png',
+      name: "Ride",
+      icon: "/Ambulance4.png",
     },
   ];
 
   return (
     <div>
-      <div className='p-5 pb-3 pl-10 border-b-[4px] border-gray-200 flex items-center justify-between'>
-        <div className='flex gap-24 items-center'>
-          <span className="text-3xl font-bold text-gray-800">Ambu<span className="text-red-500">X</span></span>
-          <div className='flex gap-6 items-center'>
+      <div className="p-5 pb-3 pl-10 border-b-[4px] border-gray-200 flex items-center justify-between">
+        <div className="flex gap-24 items-center">
+          <span className="text-3xl font-bold text-gray-800">
+            Ambu<span className="text-red-500">X</span>
+          </span>
+          <div className="flex gap-6 items-center">
             {headerMenu.map((item) => (
-              <div key={item.id} className='flex gap-2 items-center'> {/* Use item.id as the key */}
-                <Image src="/ambulance4.png" width={25} height={25} alt={item.name} />
+              <div key={item.id} className="flex gap-2 items-center">
+                {" "}
+                {/* Use item.id as the key */}
+                <Image
+                  src="/Ambulance4.png"
+                  width={25}
+                  height={25}
+                  alt={item.name}
+                />
                 <h2>{item.name}</h2>
               </div>
             ))}
           </div>
         </div>
-        <UserButton afterSignOutUrl='/' />
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
